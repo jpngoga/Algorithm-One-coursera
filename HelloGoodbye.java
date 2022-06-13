7f6933dcf69c35da4df006d69cbdef7a7b1708b0
jpngoga <55206642+jpngoga@users.noreply.github.com>
2022-06-13 20:02:20 +0200

    Create HelloGoodbye.java

---
+++ HelloGoodbye.java
@@ -0,0 +1,17 @@
+*  % java HelloGoodbye Ngojepabilly
+ ******************************************************************************/
+
+
+public class HelloGoodbye {
+	public static void main( String [] args ){
+		 System.out.print( "Hello " );
+		 System.out.print( args[0] );
+		 System.out.print( " and " );
+		 System.out.println( args[1] + "." );
+		 System.out.print( "Goodbuy " );
+		 System.out.print( args[1] );
+		 System.out.print( " and " );
+		 System.out.println( args[0] + "." );
+	
+	}
+}
