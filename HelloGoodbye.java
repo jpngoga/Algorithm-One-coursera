0035d0c63baa03b38e7b19bbf7df21f812298deb
jpngoga <55206642+jpngoga@users.noreply.github.com>
2022-06-14 17:08:02 +0200

    Update HelloGoodbye.java

--- HelloGoodbye.java
+++ HelloGoodbye.java
@@ -2,9 +2,8 @@
  ******************************************************************************/
 
 
-public class Main
-{
-	public static void main(String[] args) {
-		System.out.println("Hello, Goodbye");
-	}
+class HelloGoodbye {
+    public static void main(String[] args) {
+        System.out.println("Hello, Goodbye"); 
+    }
 }
